764ea2d0156bad765003d64dd02bc60201bbdb1d
Jaman-dedy <jeandedieuam@gmail.com>
2024-02-20 21:54:43 +0100

    dispaly products

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,26 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../services/products.service';
-import { Products } from '../../type';
+import { Product, Products } from '../../type';
+import { ProductComponent } from '../components/product/product.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   constructor(private productsService: ProductsService) {}
 
+  products: Product[]= []
+
   ngOnInit() {
     this.productsService
       .getProducts('http://localhost:3000/clothes', { page: 0, perPage: 5 })
       .subscribe((products: Products) => {
-        console.log('products :>> ', products.items);
+        this.products = products.items
       });
   }
 }
